c6f0d69502af319c4ab1fb15bfc057348a781f47
Kengo Sato <satoken@bio.keio.ac.jp>
2019-12-02 15:02:42 +0900

    Revert "fix attention"
    This reverts commit 7b6f036f00accfb4a24ceeea959d21df1e8c457b.

--- dnnfold/fold/layers.py
+++ dnnfold/fold/layers.py
@@ -74,15 +74,14 @@ class CNNPairedLayer(nn.Module):
                 nn.Dropout(p=dropout_rate)
             ]
             n_in = n_out
-        self.conv = nn.Sequential(*conv) if len(conv) > 0 else None #nn.Identity()
+        self.conv = nn.Sequential(*conv) if len(conv) > 0 else nn.Identity()
 
     
     def forward(self, x): # (B, N, N, n_in)
-        if self.conv is not None:
-            B, N, _, _ = x.shape
-            x = x.permute(0, 3, 1, 2)
-            x = self.conv(x)
-            x = x.permute(0, 2, 3, 1).view(B, N, N, -1)
+        B, N, _, _ = x.shape
+        x = x.permute(0, 3, 1, 2)
+        x = self.conv(x)
+        x = x.permute(0, 2, 3, 1).view(B, N, N, -1)
         return x # (B, N, N, n_out)
 
 
@@ -122,15 +121,14 @@ class CNNUnpairedLayer(nn.Module):
                 nn.Dropout(p=dropout_rate)
             ]
             n_in = n_out
-        self.conv = nn.Sequential(*conv) if len(conv) > 0 else None #nn.Identity()
+        self.conv = nn.Sequential(*conv) if len(conv) > 0 else nn.Identity()
 
     
     def forward(self, x): # (B, N, n_in)
-        if self.conv is not None:
-            B, N, _ = x.shape
-            x = x.permute(0, 2, 1)
-            x = self.conv(x)
-            x = x.permute(0, 2, 1).view(B, N, -1)
+        B, N, _ = x.shape
+        x = x.permute(0, 2, 1)
+        x = self.conv(x)
+        x = x.permute(0, 2, 1).view(B, N, -1)
         return x # (B, N, n_out)
 
 
